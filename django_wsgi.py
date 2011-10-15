8fbdcb5cda3b2c97cc57aa71cb37f8517b04bcfa
Jean Sanchez <jean.sanchez@intersys.mx>
2011-10-15 14:14:06 -0500

    Cambios en directorio

--- django_wsgi.py
+++ django_wsgi.py
@@ -1,10 +1,10 @@
 import os, sys
 import django.core.handlers.wsgi
 
-sys.path.insert(0, '/export/web/intersys')
-sys.path.insert(0, '/export/web/intersys/intersys')
+sys.path.insert(0, '/export/web/intersys.mx')
+sys.path.insert(0, '/export/web/intersys.mx/intersys')
 
-os.environ['PROJECT_DIR'] = '/export/web/intersys/intersys'
+os.environ['PROJECT_DIR'] = '/export/web/intersys.mx/intersys'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 os.environ['PYTHON_EGG_CACHE'] = '/tmp'
