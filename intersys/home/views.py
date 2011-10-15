a17bbcf0a2a5454cbadcb45e4cb10faff0e3e7a6
Jean Sanchez <jean.sanchez@intersys.mx>
2011-10-15 09:38:28 -0500

    cambios

---
+++ intersys/home/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+# Create your views here.
+def index(request):
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    return render_to_response('home.html')
