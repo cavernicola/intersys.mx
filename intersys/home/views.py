da4904a39c41a9a48bd67c2058fefc209c9c65d5
Jean Sanchez <jean.sanchez@intersys.mx>
2011-10-26 00:16:40 -0500

    modified:   home/views.py 	modified:   media/css/style.css 	modified:   templates/home.html 	modified:   urls.py 	media/ 	static/

--- intersys/home/views.py
+++ intersys/home/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render_to_response
 def index(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
     return render_to_response('home.html')
+
+  
+def suscribir(request):
+	if request.is_ajax():
+		#return render_to_response('home.html',{'resultado': 'hola'})
+		return HttpResponse('hola')
