1e05f68ca188a2b72696763ce60760806a338bef
Jean Sanchez <jean.sanchez@intersys.mx>
2011-10-27 22:23:42 -0500

    modified:   intersys/home/views.py 	modified:   intersys/settings.py 	modified:   intersys/templates/home.html

--- intersys/home/views.py
+++ intersys/home/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 def index(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
     return render_to_response('home.html')
 
-  
 def suscribir(request):
 	if request.is_ajax():
 		#return render_to_response('home.html',{'resultado': 'hola'})
--- intersys/settings.py
+++ intersys/settings.py
@@ -2,7 +2,7 @@
 import os.path
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
